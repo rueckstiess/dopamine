c670abbf7f838e614dd6daf7af04522242ca8939
Thomas Rückstieß <ruecksti@in.tum.de>
2010-04-30 15:36:19 +0200

    implemented giveReward, getAction, calcAction and others. added __init__.py file to agents folder.

--- agents/agent.py
+++ agents/agent.py
@@ -1,25 +1,74 @@
-from rllib.tools.dataset import DataSet, EpisodicDataSet
+from rllib.tools import Episode, History
+from numpy import zeros
+
+class AgentException(Exception):
+    pass
 
 class Agent(object):
     
-    def __init__(self):
-        self.dataset = 
-    
+    def __init__(self, stateDim, actionDim):
+        # store state and action dimensions
+        self.stateDim = stateDim
+        self.actionDim = actionDim
+        
+        # create history to store experiences
+        self.history = History(stateDim, actionDim)
+        
+        # current observation, action, reward
+        self.obs = None
+        self.action = None
+        self.reward = None
+        
+        # progress counter:
+        # 0 = reward was given, before observation was integrated
+        # 1 = integration was integrated, before action was returned
+        # 2 = action was returned, before reward was given
+        # 0 = reward was given. store experience in history
+        self.progressCnt = 0
+        
+    @property
+    def episode(self):
+        """ returns the last (current) episode. """
+        return self.history[-1]
+        
     def integrateObservation(self, obs):
-        pass
-        
+        if self.progressCnt == 0:
+            self.obs = obs
+            self.progressCnt = 1
+        else:
+            raise AgentException('observation was given twice before action was requested.')
+                
     def getAction(self):
-        pass
+        if self.progressCnt == 1:
+            self.calcAction()
+            self.progressCnt = 2
+            return self.action
+        else:
+            if self.progressCnt == 0:
+                raise AgentException('action was requested before observation was integrated.')
+            if self.progressCnt > 1:
+                raise AgentException('action was requested after reward was given.')
         
-    def giveReward(self):
-        pass
+    def giveReward(self, reward):
+        if self.progressCnt == 2:
+            self.reward = reward
+            self.progressCnt = 0
+            self.history.append(self.obs, self.action, self.reward)
+        else:
+            raise AgentException('reward was requested before action was returned.')
         
+    def newEpisode(self):
+        self.history.newEpisode()
+    
     def explore(self):
         pass
         
     def learn(self):
         pass
         
+    def calcAction(self):
+        """ this method needs to be overwritten by subclasses to return a non-zero action. """
+        self.action = zeros(self.actionDim)
     
     
     
