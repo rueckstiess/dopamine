7a00495893f7ffeeb71432ddb8046204435192a5
Thomas Rückstieß <ruecksti@in.tum.de>
2010-05-02 15:41:24 +0200

    q-learning not implemented yet.

--- agents/valuebased/q.py
+++ agents/valuebased/q.py
@@ -2,20 +2,24 @@ from rllib.agents import Agent
 from rllib.agents.valuebased import Table
 
 class QAgent(Agent):
+    pass
     
-    def learn(self):
-        for episode in self.history:
-            for state, action, reward, nextstate in episode:
-            
-                state = int(state)
-                action = int(action)
-                nextstate = int(nextstate)
-     
-                qvalue = self.module.getValue(self.laststate, self.lastaction)
-                maxnext = self.module.getValue(state, self.module.getMaxAction(state))
-                self.module.updateValue(self.laststate, self.lastaction, qvalue + self.alpha * (reward + self.gamma * maxnext - qvalue))
-    
-                # move state to oldstate
-                self.laststate = state
-                self.lastaction = action
+    # def __init__(self):
+    #     
+    #     
+    # def learn(self):
+    #     for episode in self.history:
+    #         for state, action, reward, nextstate in episode:
+    #         
+    #             state = int(state)
+    #             action = int(action)
+    #             nextstate = int(nextstate)
+    #  
+    #             qvalue = self.module.getValue(self.laststate, self.lastaction)
+    #             maxnext = self.module.getValue(state, self.module.getMaxAction(state))
+    #             self.module.updateValue(self.laststate, self.lastaction, qvalue + self.alpha * (reward + self.gamma * maxnext - qvalue))
+    # 
+    #             # move state to oldstate
+    #             self.laststate = state
+    #             self.lastaction = action
     
