b7a12055b69b724ddba7585388052315e88110ff
Thomas Rückstieß <ruecksti@in.tum.de>
2010-05-04 09:42:24 +0200

    plot q-table in maze after every episode. added cart_nfq script.

--- scripts/maze_q.py
+++ scripts/maze_q.py
@@ -4,19 +4,23 @@ from rllib.experiments import Experiment
 from rllib.adapters import MakeEpisodicAdapter, EpsilonGreedyExplorer
 
 from matplotlib import pyplot as plt
+import time
 
 agent = QAgent()
 environment = MDPMaze()
 experiment = Experiment(environment, agent)
 experiment.addAdapter(MakeEpisodicAdapter(100))
 
-explorer = EpsilonGreedyExplorer(0.2, 0.9999)
+explorer = EpsilonGreedyExplorer(0.3, 0.9999)
 experiment.addAdapter(explorer)
+
+plt.ion()
+plt.gray()
 
 for i in range(100):
     experiment.runEpisode(reset=True)
-agent.learn()
-
-plt.pcolor(agent.table.values.max(axis=1).reshape(9, 9))
-plt.gray()
-plt.show()
+    agent.learn()
+    agent.forget()
+    print explorer.epsilon
+    plt.pcolor(agent.estimator.values.max(axis=1).reshape(9, 9))
+    plt.gcf().canvas.draw()
