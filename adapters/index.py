752a601af0c8f26b0c4cc9e8924e7189ddbbab1a
Thomas Rückstieß <ruecksti@in.tum.de>
2010-05-17 13:28:03 +0200

    added description to IndexingAdapter

--- adapters/index.py
+++ adapters/index.py
@@ -2,7 +2,19 @@ from rllib.adapters import Adapter
 from numpy import array, inf, ones
 
 class IndexingAdapter(Adapter):
-    """ 
+    """ This adapter chooses selectively from the state and action vector
+        and only passes certain indices of these vectors. Specify the
+        indices in the initialization. Pass None, if you don't want the
+        state or action vector indexed.
+        
+        Example:
+            IndexingAdapter([1, 3, 5], [0, 1])
+            
+            This adapter would only pass state indices 1, 3 and 5 to the
+            agent, thus making it a 3-dimensional environment. it would also
+            only pass action indices 0 and 1 back to the agent. Note: state
+            indexing is much more common (e.g. to hide certain information
+            from the agent).
     """
     
     # can be applied to all conditions
