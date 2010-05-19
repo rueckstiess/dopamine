3f81af9cbc84b36ba46bf7be5ef4f08b4c89722e
Thomas Rückstieß <ruecksti@in.tum.de>
2010-05-19 14:57:32 +0200

    added a switch "active" to explorers to turn them on and off.

--- adapters/explorers/explorer.py
+++ adapters/explorers/explorer.py
@@ -8,10 +8,18 @@ class Explorer(Adapter):
     # define the conditions of the environment
     outConditions = {}
             
+    def __init__(self):
+        Adapter.__init__(self)
+        
+        # set this to False to turn off exploration
+        self.active = True        
+    
     def applyAction(self, action):
         """ apply transformations to action and return it. """
-        action = self._explore(action)
-        self.experiment.agent.action = action
+        if self.active:
+            action = self._explore(action)
+            self.experiment.agent.action = action
+        
         return action
     
     def _explore(self, action):
