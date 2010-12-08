581aa0e6938934417f6cbaeef2821333ddfce692
Thomas Rückstieß <thomas@rueckstiess.net>
2010-12-08 14:22:00 +0100

    changed to match new FAEstimator.

--- scripts/cart_fqi_rbf.py
+++ scripts/cart_fqi_rbf.py
@@ -1,30 +1,36 @@
 from dopamine.environments import DiscreteCartPoleEnvironment, CartPoleRenderer
-from dopamine.agents import FQIAgent, RBFEstimator, NNEstimator, RBFOnlineEstimator, LWPREstimator
+from dopamine.agents import FQIAgent
+from dopamine.fapprox import *
 from dopamine.experiments import Experiment
-from dopamine.adapters import EpsilonGreedyExplorer, NormalizingAdapter, IndexingAdapter
+from dopamine.adapters import EpsilonGreedyExplorer, NormalizingAdapter, IndexingAdapter, RewardBaselineAdapter
 
 from matplotlib import pyplot as plt
 from numpy import *
 
 # create agent, environment, renderer, experiment
-agent = FQIAgent(estimatorClass=RBFEstimator)
+agent = FQIAgent(faClass=RBF)
 agent.iterations = 1
+agent.presentations = 1
 environment = DiscreteCartPoleEnvironment()
 environment.conditions['actionNum'] = 2
 environment.centerCart = False
 experiment = Experiment(environment, agent)
 
 # cut off last two state dimensions
-indexer = IndexingAdapter([0, 1], None)
-experiment.addAdapter(indexer)
+# indexer = IndexingAdapter([0, 1], None)
+# experiment.addAdapter(indexer)
 
 # add normalization adapter
 normalizer = NormalizingAdapter()
 experiment.addAdapter(normalizer)
 
 # add e-greedy exploration
-explorer = EpsilonGreedyExplorer(0.3, 0.99995)
+explorer = EpsilonGreedyExplorer(0.3)
 experiment.addAdapter(explorer)
+
+experiment.setup()
+explorer.epsilon = 0.5
+explorer.decay = 0.9999
 
 # renderer = CartPoleRenderer()
 # environment.renderer = renderer
@@ -32,14 +38,17 @@ experiment.addAdapter(explorer)
     
 # run experiment
 for i in range(1000):
-    experiment.runEpisodes(1)
-    agent.learn()
-    # agent.history.truncate(20)
-    # agent.forget()
-    
-    valdata = experiment.evaluateEpisodes(20, visualize=True)
+    valdata = experiment.evaluateEpisodes(10, visualize=True)
     mean_return = mean([sum(v.rewards) for v in valdata])
     print "exploration", explorer.epsilon
     print "mean return", mean_return
+    print "mean ep. length", mean([len(e) for e in valdata])
     print "num episodes", len(agent.history)
     print "num total samples", agent.history.numTotalSamples()
+    print
+
+    experiment.runEpisodes(1)
+    agent.learn()
+    agent.history.truncate(50)
+
+    
