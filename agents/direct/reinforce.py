c5b1276847bbdca14b9f975b575db7f61f0abf47
Thomas Rückstieß <thomas@rueckstiess.net>
2010-12-09 16:51:40 +0100

    calling new function dOutdTheta from the parametric fa's.

--- agents/direct/reinforce.py
+++ agents/direct/reinforce.py
@@ -8,7 +8,7 @@ class ReinforceAgent(DirectAgent):
  
     alpha = 10e-2
     alpha_sigma = 10e-3
-    sigmadecay = 0.05
+    sigmadecay = 0.005
                           
     def learn(self):
         # create the derivative of the log likelihoods for each timestep in each episode
@@ -21,8 +21,8 @@ class ReinforceAgent(DirectAgent):
                 # calculate dpi/dmu
                 rl_error = array((a - self.controller.predict(s))).reshape(1, self.conditions['actionDim'])
                 # calculate dpi/dtheta
-                derivFkt = self.controller.getDerivative(s)
-                inner_loglh_mu.append(derivFkt(rl_error))
+                inner_loglh_mu.append(self.controller.dOutdTheta(s, rl_error))
+                # print inner_loglh_mu[-1]
             loglh_mu.append(inner_loglh_mu)
         
         baseline = mean([sum(loglh_mu[ie], axis=0)**2 * mean(e.rewards) for ie, e in enumerate(self.history)], axis=0) / mean([sum(loglh_mu[ie], axis=0)**2 for ie, e in enumerate(self.history)], axis=0)
@@ -30,7 +30,7 @@ class ReinforceAgent(DirectAgent):
         
         # update parameters of controller
         self.controller.parameters = self.controller.parameters + self.alpha * gradient.flatten()
-        
+        print gradient
         
         for explorer in self.experiment.explorers:
             if hasattr(explorer, 'sigmaAdaptation') and explorer.sigmaAdaptation:
@@ -46,7 +46,7 @@ class ReinforceAgent(DirectAgent):
         
                 baseline = mean([sum(loglh_sigma[ie], axis=0)**2 * mean(e.rewards) for ie, e in enumerate(self.history)], axis=0) / mean([sum(loglh_sigma[ie], axis=0)**2 for ie, e in enumerate(self.history)], axis=0)
                 gradient = mean([sum(loglh_sigma[ie], axis=0) * ((mean(e.rewards) - baseline)) for ie, e in enumerate(self.history)], axis=0)
-        
+            
                 # update parameters of explorer
                 explorer.sigma = explorer.sigma + self.alpha_sigma * gradient.flatten()
             else:
