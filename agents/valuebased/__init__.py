a26eba253538f9b8f4196028b970721034815a0b
Thomas Rückstieß <thomas@rueckstiess.net>
2011-02-02 08:50:46 +0100

    included OrderedFAEstimator and OrderedFQIAgent.

--- agents/valuebased/__init__.py
+++ agents/valuebased/__init__.py
@@ -4,9 +4,10 @@ from sarsa import SARSAAgent
 
 # continuous agents
 from fqi import FQIAgent
+from orderedfqi import OrderedFQIAgent
 from fmci import FMCIAgent
 from bas import BASAgent
 
 # estimators
 from table import TableEstimator
-from faestimator import FAEstimator
+from faestimator import FAEstimator, OrderedFAEstimator
--- agents/valuebased/faestimator.py
+++ agents/valuebased/faestimator.py
@@ -1,6 +1,6 @@
 from numpy import *
 from dopamine.agents.valuebased.estimator import Estimator
-from dopamine.fapprox import RBF
+from dopamine.fapprox import RBF, Linear
 
 from matplotlib import pyplot as plt
 
@@ -40,4 +40,33 @@ class FAEstimator(Estimator):
         """ train individual models for each actions seperately. """
         for a in range(self.actionNum):
             self.fas[a].train()
-     
+
+
+
+class OrderedFAEstimator(FAEstimator):
+    """ This estimator allows actions to be chosen only once during an episode.
+        It will manually assign -inf to the value of an already chosen action,
+        making sure the action will not be chosen again, until resetMemory
+        is called. The already selected actions have to be specified explicitly
+        via rememberAction().
+    """
+
+    def __init__(self, stateDim, actionNum, faClass=Linear):
+        """ initialize with the state dimension and number of actions. """
+        FAEstimator.__init__(self, stateDim, actionNum, faClass)
+        self.memory = []
+
+    def getValue(self, state, action):
+        """ returns the value of the given (state,action) pair as float. """
+        action = int(action.item())
+        if action in self.memory:
+            return -inf
+        else:
+            state = state.flatten()
+            return self.fas[action].predict(state).item()
+
+    def rememberAction(self, action):
+        self.memory.append(int(action.item()))
+
+    def resetMemory(self):
+        self.memory = []
