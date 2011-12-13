a27cdb48eb46f5c11bcc84fa53d259a7c8d8cee5
Thomas Rueckstiess <thomas@rueckstiess.net>
2011-12-14 00:20:04 +0800

    some changes in the api example.

--- scripts/cart_api_linear.py
+++ scripts/cart_api_linear.py
@@ -8,17 +8,17 @@ from matplotlib import pyplot as plt
 from numpy import *
 
 # create agent, environment, renderer, experiment
-agent = APIAgent()
+agent = APIAgent(resetFA=False)
 agent.iterations = 1
 
-environment = DiscreteCartPoleEnvironment(maxSteps=100)
+environment = DiscreteCartPoleEnvironment(maxSteps=200)
 environment.conditions['actionNum'] = 2
 environment.centerCart = False
 experiment = APIExperiment(environment, agent)
 
 # cut off last two state dimensions
-indexer = IndexingAdapter([0, 1], None)
-experiment.addAdapter(indexer)
+# indexer = IndexingAdapter([0, 1], None)
+# experiment.addAdapter(indexer)
 
 # add normalization adapter
 normalizer = NormalizingAdapter()
@@ -34,19 +34,19 @@ explorer.decay = 0.9999
 
 renderer = CartPoleRenderer()
 
-valdata = experiment.evaluateEpisodes(100, visualize=False)
-mean_return = mean([sum(v.rewards) for v in valdata])
-print "mean return", mean_return
+for i in range(1):
+    valdata = experiment.evaluateEpisodes(100, visualize=False)
+    mean_return = mean([sum(v.rewards) for v in valdata])
+    print "mean return", mean_return
+    print "exploration", explorer.epsilon
+    print "mean ep. length", mean([len(e) for e in valdata])
+    print "num episodes", len(agent.history)
+    print "num total samples", agent.history.numTotalSamples()
+    print
 
-# print "exploration", explorer.epsilon
-# print "mean return", mean_return
-# print "mean ep. length", mean([len(e) for e in valdata])
-# print "num episodes", len(agent.history)
-# print "num total samples", agent.history.numTotalSamples()
-# print
-
-experiment.runEpisodes(100)
-agent.learn()
+    experiment.runEpisodes(500)
+    agent.learn()
+    agent.forget()
 
 valdata = experiment.evaluateEpisodes(100, visualize=False)
 mean_return = mean([sum(v.rewards) for v in valdata])
